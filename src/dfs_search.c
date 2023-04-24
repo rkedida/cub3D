d0cbe0fc782b77abf70ee9ea53e823b3b6e894f7
Shetima kyari Ali <shettimaali58@yahoo.com>
2023-04-24 15:17:27 +0200

    more clean ups

--- src/dfs_search.c
+++ src/dfs_search.c
@@ -6,31 +6,16 @@
 /*   By: sheali <sheali@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 23:11:09 by rkedida           #+#    #+#             */
-/*   Updated: 2023/04/21 01:25:15 by sheali           ###   ########.fr       */
+/*   Updated: 2023/04/24 15:09:35 by sheali           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-// void	dfs(t_mapData *Map, int row, int col, bool **visited)
-// {
-// 	visited[row][col] = true;
-// 	// if (Map->map[row][col] == 'C')
-// 	// 	Map->num_collectibles--;
-// 	if (Map->map[row][col] == '1')
-// 		Map->found_exit = true;
-// 	up(Map, row, col, visited);
-// 	down(Map, row, col, visited);
-// 	left(Map, row, col, visited);
-// 	right(Map, row, col, visited);
-// }
-
 void	leaks(void)
 {
 	system("leaks cub3D");
 }
-
-//-------------------------------TEST--------------------------------------
 
 void	ft_mlx_pixel_put(t_img *texture, int x, int y, int color)
 {
